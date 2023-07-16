848eddb6e9d48cf6226c11f693422b146572e239
Makatia13 <kelvinmakatia13@gmail.com>
2023-07-16 10:30:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program tht prints a line with puts function
+ * Return : 0 (Succes)
+ */
+int (void)
+{
+	puts("\"Programming is like building a multingual puzzle");
+	return (0);
+}
